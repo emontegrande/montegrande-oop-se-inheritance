214c06f27f026bc822a3b405fba643806d05a4ce
emontegrande <montegrandeelouiseit1a@gmail.com>
2017-06-23 16:18:09 +0800

    hi

---
+++ ADemo.java
@@ -0,0 +1,10 @@
+package ConstructorBehavior;
+public class ADemo {
+    public static void main(String[] args) {
+        A a = new A();
+        A a1 = new A("hi");
+        A a2 = new A(2);
+        A a3 = new A("hi", 2);
+        
+    }
+}
---
+++ BDemo.java
@@ -0,0 +1,7 @@
+package ConstructorBehavior;
+public class BDemo {
+    public static void main(String[] args) {
+        B b = new B();
+        B b1 = new B(true);
+    }
+}
